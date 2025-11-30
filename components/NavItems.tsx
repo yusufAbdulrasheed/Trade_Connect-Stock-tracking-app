578818319a50f62a66715e4f532799821d6ef5d2
Yusuf Abdulrasheed <yusufabdulrasheed200@gmail.com>
2025-11-30 15:54:04 +0100

    implemented the search command and watchlist

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,28 +1,44 @@
-"use client"
-import { NAV_ITEMS } from "@/lib/constants"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+"use client";
+import { NAV_ITEMS } from "@/lib/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import SearchCommand from "./SearchCommand";
 
 const NavItems = () => {
+  const pathname = usePathname();
 
-    const pathname = usePathname()
+  const isActive = (path: string) => {
+    if (pathname === "/") return pathname === "/";
 
-    const isActive = (path: string) =>{
-        if(pathname === '/') return pathname === '/' ;
-
-        return pathname.startsWith(path)
-    }
+    return pathname.startsWith(path);
+  };
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-        {NAV_ITEMS.map(({href, label}) =>(
-            <li key={href} className="nav-item">
-                <Link href={href} className={`hover:text-yellow-500 transition-colors ${ isActive(href) ? 'text-gray-100' : ''}`}>
-                    {label}
-                </Link>
-            </li>
-        ))}
-    </ul>
-  )
-}
+      {NAV_ITEMS.map(({ href, label }) => {
 
-export default NavItems
+        if(label === 'Search') return (
+            <li key="search-trigger">
+                <SearchCommand
+                    renderAs="text"
+                    label="Search"
+                    initialStocks={[]}
+                />
+            </li>
+        )
+
+        return <li key={href} className="nav-item">
+          <Link
+            href={href}
+            className={`hover:text-yellow-500 transition-colors ${
+              isActive(href) ? "text-gray-100" : ""
+            }`}
+          >
+            {label}
+          </Link>
+        </li>;
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
