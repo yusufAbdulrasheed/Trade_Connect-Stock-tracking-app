4055fb1df583a4889e55055d63de10c1c70558c3
Yusuf Abdulrasheed <yusufabdulrasheed200@gmail.com>
2025-12-01 13:08:02 +0100

    tried adding new background to the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
 import UserDropDown from "./userDropdown";
-import {searchStocks} from "@/lib/actions/finnhub.actions"
+import { searchStocks } from "@/lib/actions/finnhub.actions";
 
 const Header = async ({ user }: { user: User | null }) => {
-  const initialStocks = await searchStocks()
+  const initialStocks = await searchStocks();
   return (
     <header className="sticky top-0 z-50 w-full bg-black/40 backdrop-blur-md">
       <div className="container mx-auto flex items-center justify-between px-4 py-3 md:py-4">
-        
         {/* Left Section: Logo + Desktop Nav */}
         <div className="flex items-center gap-4 md:gap-8">
           <Link href="/" className="flex items-center gap-2">
-            <div className="relative flex h-9 w-9 items-center justify-center rounded-xl">
+            <div className="relative flex h-9 w-9 items-center justify-center rounded-xl bg-gray-700/40 backdrop-blur">
               <Image
                 src="/assets/icons/logo-1.svg"
                 alt="Trade Connect"
@@ -31,14 +30,14 @@ const Header = async ({ user }: { user: User | null }) => {
 
           {/* Desktop Nav */}
           <nav className="hidden md:block">
-            <NavItems initialStocks={initialStocks}/>
+            <NavItems initialStocks={initialStocks} />
           </nav>
         </div>
 
         {/* Right Section: User or Auth buttons */}
         <div className="flex items-center gap-2 sm:gap-3">
           {user ? (
-            <UserDropDown user={user} initialStocks={initialStocks}/>
+            <UserDropDown user={user} initialStocks={initialStocks} />
           ) : (
             <div className="flex items-center gap-2">
               <Link
@@ -57,7 +56,6 @@ const Header = async ({ user }: { user: User | null }) => {
             </div>
           )}
         </div>
-
       </div>
     </header>
   );
