38cebd9b08972cd6c032b65e232957036bc1c863
Yusuf Abdulrasheed <yusufabdulrasheed200@gmail.com>
2025-12-02 17:44:05 +0100

    updated inngest function

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,6 +1,6 @@
 import { Inngest } from 'inngest'
 
 export const inngest = new Inngest({
-    id: 'tradeconnect',
+    id: 'signalist',
     ai: { gemini: { apiKey: process.env.GEMINI_API_KEY}}
 })
